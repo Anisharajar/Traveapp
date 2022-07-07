da9c6df9ab639466f599fb169342b829b165cac7
Anisharajar <anisharajar.2002@gmail.com>
2022-07-08 01:12:35 +0530

    Travelreg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TravelregComponent } from './travelreg/travelreg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TravelregComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/travelreg/travelreg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-travelreg',
+  templateUrl: './travelreg.component.html',
+  styleUrls: ['./travelreg.component.css']
+})
+export class TravelregComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
